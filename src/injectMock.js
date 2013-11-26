95843659a970d4b262208bc917af681be13dbad3
Takeshi SASAKI <takeshi.sasaki@quipper.com>
2013-11-26 20:48:41 +0900

    avoid adding two or more script elements

--- src/injectMock.js
+++ src/injectMock.js
@@ -1,9 +1,13 @@
 // Content script
 
 function injectScript() {
-    scriptElem = document.createElement('script');
-    scriptElem.src = chrome.extension.getURL("mockDate.js")
-    document.head.appendChild(scriptElem);
+    var script = document.getElementById('mo-ckda-te')
+    if (!script) {
+        scriptElem = document.createElement('script');
+        scriptElem.src = chrome.extension.getURL("mockDate.js");
+        scriptElem.id = 'mo-ckda-te';
+        document.head.appendChild(scriptElem);
+    }
 }
 
 function receiveSetting(request, sender, sendResponse) {
